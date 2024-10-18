a29123692b89dbabbc21beff7603c2225acd305a
Suvadip-sana <suvadipsana602@gmail.com>
2024-10-18 23:44:44 +0530

    Update script

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let mobNav = document.querySelector(".mobile-navbar");
 let close = document.querySelector(".close");
 let menuLogo = document.querySelector(".menuLogo");
+let backToTop = document.querySelector(".go-top");
 
 menuLogo.addEventListener("click", () => {
     mobNav.classList.add("open");
-})
+});
 
 close.addEventListener("click", () => {
     mobNav.classList.remove("open");
-})
+});
+
+window.addEventListener("scroll", () => {
+    // console.log(window);
+    let scrollPosition = window.scrollY;
+    scrollPosition > 400 ? backToTop.style.display = "flex" : backToTop.style.display = "none";
+});
+
+backToTop.addEventListener("click", () => {
+    window.scrollTo({top: 0, behavior: "smooth"});
+});
